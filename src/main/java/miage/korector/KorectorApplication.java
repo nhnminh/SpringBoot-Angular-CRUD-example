16b54af6c8ba53f32fc9675624901b07ac55ba5a
Thuy Dung LE <lethuydung0109@gmail.com>
2020-03-12 18:07:26 +0100

    First commit - Finish part 1 de tuto

---
+++ src/main/java/miage/korector/KorectorApplication.java
@@ -0,0 +1,13 @@
+package miage.korector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KorectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KorectorApplication.class, args);
+	}
+
+}
